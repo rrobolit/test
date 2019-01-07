e8aa8e163b29bddf57dccaa02281f28c538650b1
rrobolit <46083876+rrobolit@users.noreply.github.com>
2019-01-07 21:40:54 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python-package-example',
+    version='0.1',
+    packages=find_packages(),
+    license='MIT',
+    description='An example python package',
+    long_description='qqq',
+    install_requires=[],
+    url='https://github.com/BillMills/python-package-example',
+    author='',
+    author_email=''
+)
